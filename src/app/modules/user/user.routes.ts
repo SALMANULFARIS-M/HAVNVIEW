4331121458755e6484a29edb65419c012ade4698
Salmanulfaris-M <fsalmanul13@gmail.com>
2025-07-01 15:08:29 +0530

    changed the background with some svgs

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from "./home/home.component";
 import { ContactComponent } from "./contact/contact.component";
 import { AboutComponent } from "./about/about.component";
 import { PropertiesComponent } from "./properties/properties.component";
-import { RegisterComponent } from "./register/register.component";
 import { AuthComponent } from "../../core/authentication/auth/auth.component";
 
 
@@ -16,5 +15,4 @@ export const USER_ROUTES: Routes = [
     path: 'auth/:mode',
     component: AuthComponent
   },
-  { path: 'register', component: RegisterComponent },
 ];
